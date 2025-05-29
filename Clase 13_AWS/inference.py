48c582d1da761f24f6fae83381872a8aa0d888a0
JennyReaP <jenny.rea@yachaytech.edu.ec>
2025-05-28 20:51:23 -0500

    Update inference.py

--- Clase 13_AWS/inference.py
+++ Clase 13_AWS/inference.py
@@ -3,7 +3,7 @@ import joblib
 # Datos fijos para prueba
 features = [[1, 45, 58, 11, 22, 0]]
 # Cargar el modelo
-modelo = joblib.load("modelo.pkl")
+modelo = joblib.load("modeloPinguino.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
